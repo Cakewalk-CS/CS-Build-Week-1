8acfdfdb4c5ee8157f95c8e0298be7c5e8fe90ca
chad kidd <ckiddwebdev@gmail.com>
2020-01-07 00:08:10 -0800

    added whitenoise middleware to settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -55,6 +55,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +149,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# compression and caching support
+# source http://whitenoise.evans.io/en/stable/django.html
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Make sure staticfiles is configured correctly
+# source http://whitenoise.evans.io/en/stable/django.html
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 import django_heroku
 django_heroku.settings(locals())
--- adventure/api.py
+++ adventure/api.py
@@ -20,13 +20,19 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    return JsonResponse({
+    'uuid': uuid, 
+    'name':player.user.username, 
+    'title':room.title, 
+    'description':room.description, 
+    'players':players}, 
+    safe = True)
 
 
 # @csrf_exempt
 @api_view(["POST"])
 def move(request):
-    dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
+    dirs = {"n": "north", "s": "south", "e": "east", "w": "west"}
     reverse_dirs = {"n": "south", "s": "north", "e": "west", "w": "east"}
     player = request.user.player
     player_id = player.id
@@ -44,8 +50,8 @@ def move(request):
     elif direction == "w":
         nextRoomID = room.w_to
     if nextRoomID is not None and nextRoomID > 0:
-        nextRoom = Room.objects.get(id=nextRoomID)
-        player.currentRoom=nextRoomID
+        nextRoom = Room.objects.get(id = nextRoomID)
+        player.currentRoom = nextRoomID
         player.save()
         players = nextRoom.playerNames(player_id)
         currentPlayerUUIDs = room.playerUUIDs(player_id)
@@ -54,14 +60,27 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+        return JsonResponse({
+            'name':player.user.username, 
+            'title':nextRoom.title, 
+            'description':nextRoom.description, 
+            'players':players, 
+            'error_msg':""}, 
+            safe = True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({
+            'name':player.user.username, 
+            'title':room.title, 
+            'description':room.description, 
+            'players':players, 
+            'error_msg':"You cannot move that way."}, 
+            safe = True)
 
 
+# STRETCH GOAL
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    return JsonResponse({'error':"Not yet implemented"}, safe = True, status=500)
